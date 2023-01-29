a86e38a30ca94dbd1e153d74cf810bda1af24302
Nikolai <nikdenof@gmail.com>
2023-01-29 23:04:28 +0300

    Perfected the SegNet model

--- SegNet.py
+++ SegNet.py
@@ -1,132 +1,83 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-CONV_PADDING = 1
-CONV_STRIDE = 1
-CONV_KERNEL= 3
-POOL_STRIDE = 2
-POOL_KERNEL = 2
+BATCHNORM = True # Can be used to mesure performance difference
+K_CONV = 3 # Kernel size for convolutional layer
+P_CONV = 1 # Padding size for convolutional layer
+K_POOL = 2 # Kernel size for max polling layer
+S_POOL = 2 # Stride size for max polling layer
 
-INPUT_SIZE = 3
-OUTPUT_SIZE = 1 # Class output (0 - nothing, 1 - segmented something)
 
-BATCHNORM = False # Can be used to mesure performance difference
+def encoder_layer(in_c, out_c, depth = 2):
+    layer = []
+    for i in range(depth):
+        if i == 0:
+            layer.append(nn.Conv2d(in_c, out_c, kernel_size=K_CONV, padding=P_CONV))
+        else:
+            layer.append(nn.Conv2d(out_c, out_c, kernel_size=K_CONV, padding=P_CONV))
+        if BATCHNORM:
+            layer.append(nn.BatchNorm2d(out_c))
+        layer.append(nn.ReLU(inplace=True))
 
-def encoder_layer_s(in_c, out_c):
-    k_conv = 3 # Kernel size for convolutional layer
-    p_conv = 1 # Padding size for convolutional layer
-    if BATCHNORM:
-        layer = nn.Sequential(
-                    nn.Conv2d(in_c, out_c, kernel_size=k_conv, padding=p_conv),
-                    nn.BatchNorm2d(out_c),
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(out_c, out_c, kernel_size=k_conv, padding=p_conv),
-                    nn.BatchNorm2d(out_c),
-                    nn.ReLU(inplace=True)
-                )
-    else:
-        layer = nn.Sequential(
-                    nn.Conv2d(in_c, out_c, kernel_size=k_conv, padding=p_conv),
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(out_c, out_c, kernel_size=k_conv, padding=p_conv),
-                    nn.ReLU(inplace=True)
-                )
-    return layer
+    return nn.Sequential(*layer)
 
-def encoder_layer_l(in_c, out_c):
-    k_conv = 3 # Kernel size for convolutional layer
-    p_conv = 1 # Padding size for convolutional layer
-    if BATCHNORM:
-        layer = nn.Sequential(
-                    nn.Conv2d(in_c, out_c, kernel_size=k_conv, padding=p_conv),
-                    nn.BatchNorm2d(out_c),
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(out_c, out_c, kernel_size=k_conv, padding=p_conv),
-                    nn.BatchNorm2d(out_c),
-                    nn.ReLU(inplace=True)
-                    nn.Conv2d(out_c, out_c, kernel_size=k_conv, padding=p_conv),
-                    nn.BatchNorm2d(out_c),
-                    nn.ReLU(inplace=True)
-                )
-    else:
-        layer = nn.Sequential(
-                    nn.Conv2d(in_c, out_c, kernel_size=k_conv, padding=p_conv),
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(out_c, out_c, kernel_size=k_conv, padding=p_conv),
-                    nn.ReLU(inplace=True)
-                    nn.Conv2d(out_c, out_c, kernel_size=k_conv, padding=p_conv),
-                    nn.ReLU(inplace=True)
-                )
-    return layer
 
 def maxpool_layer():
-    k_pool = 2 # Kernel size for max polling layer
-    s_pool = 2 # Stride size for max polling layer
-    layer = nn.MaxPool2d(kernel_size=k_pool, stride=s_pool, return_indices=True)
-    
+    layer = nn.MaxPool2d(kernel_size=K_POOL, stride=S_POOL, return_indices=True)
     return layer
             
 
-def bottleneck():
-    """
-    SOMETHING WOOOOOOOOOOOOOOOOOOOOOOOOOOOO
-
-    """
-    c = 512 # The number of in and out channels in bottleneck is the same
-    k_conv = 3 # Kernel size for convolutional layer
-    p_conv = 1 # Padding size for convolutional layer
-    if BATCHNORM:
-        layer = nn.Sequential(
-                    nn.Conv2d(c, c, kernel_size=k_conv, padding=p_conv),
-                    nn.BatchNorm2d(c),
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(c, c, kernel_size=k_conv, padding=p_conv),
-                    nn.BatchNorm2d(c),
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(c, c, kernel_size=k_conv, padding=p_conv),
-                    nn.BatchNorm2d(c),
-                    nn.ReLU(inplace=True)
-                )
-    else:
-        layer = nn.Sequential(
-                    nn.Conv2d(c, c, kernel_size=k_conv, padding=p_conv),
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(c, c, kernel_size=k_conv, padding=p_conv),
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(c, c, kernel_size=k_conv, padding=p_conv),
-                    nn.ReLU(inplace=True)
-                )
-    return layer
-
-
 def unpool_layer():
-    k_pool = 2 # Kernel size for max polling layer
-    s_pool = 2 # Stride size for max polling layer
-    layer = nn.MaxUnpool2d(kernel_size=k_pool, stride=s_pool)
-    
+    layer = nn.MaxUnpool2d(kernel_size=K_POOL, stride=S_POOL)
     return layer
+
+
+def decoder_layer(in_c, out_c, depth=2, not_final=True):
+    layer = []
+    for i in range(depth):
+        if i == depth - 1:
+            layer.append(nn.Conv2d(in_c, out_c, kernel_size=K_CONV, padding=P_CONV))
+            if not_final:
+                if BATCHNORM:
+                    layer.append(nn.BatchNorm2d(out_c))
+                layer.append(nn.ReLU(inplace=True))
+        else:
+            layer.append(nn.Conv2d(in_c, in_c, kernel_size=K_CONV, padding=P_CONV))
+            if BATCHNORM:
+                layer.append(nn.BatchNorm2d(in_c))
+            layer.append(nn.ReLU(inplace=True))
+
+    return nn.Sequential(*layer)
 
 
 class SegNet(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.enc_conv0 = encoder_layer_s(3, 64)
+        # Encoder
+        self.enc_conv0 = encoder_layer(3, 64)
         self.pool0 = maxpool_layer()
-        self.enc_conv1 = encoder_layer_s(64, 128)
+        self.enc_conv1 = encoder_layer(64, 128)
         self.pool1 = maxpool_layer()
-        self.enc_conv2 = encoder_layer_l(128, 256)
+        self.enc_conv2 = encoder_layer(128, 256, depth=3)
         self.pool2 = maxpool_layer()
-        self.enc_conv3 = encoder_layer_l(256, 512)
+        self.enc_conv3 = encoder_layer(256, 512, depth=3)
         self.pool3 = maxpool_layer()
-        # Bottleneck
-        self.bottleneck_conv = bottleneck()
-        self.bottleneck_pool = maxpool_layer()
-        self.bottleneck_unpool = unpool_layer()
-        self.bottleneck_dec_conv = bottleneck()
 
-        self.upsample = unpool_layer()
+        # Bottleneck
+        self.bottlenecK_CONV = encoder_layer(512, 512, depth=3) 
+        self.bottlenecK_POOL = maxpool_layer()
+        self.bottleneck_unpool = unpool_layer()
+        self.bottleneck_dec_conv = decoder_layer(512, 512, depth=3) 
+
+        # Decoder
+        self.upsample0 = unpool_layer()
+        self.dec_conv0 = decoder_layer(512, 256, depth=3)
+        self.upsample1 = unpool_layer()
+        self.dec_conv1 = decoder_layer(256, 128, depth=3)
+        self.upsample2 = unpool_layer()
+        self.dec_conv2 = decoder_layer(128, 64)
+        self.upsample3 = unpool_layer()
+        self.dec_conv3 = decoder_layer(64, 1, not_final=False) # No activation
 
     def forward(self, x):
         # encoder
@@ -136,8 +87,8 @@ class SegNet(nn.Module):
         e3, ind3 = self.pool3(self.enc_conv3(e2))
 
         # bottleneck
-        b0 = self.bottleneck_conv(e3)
-        b1, indb = self.bottleneck_pool(b0)
+        b0 = self.bottlenecK_CONV(e3)
+        b1, indb = self.bottlenecK_POOL(b0)
         b2 = self.bottleneck_unpool(b1, indb)
         b3 = self.bottleneck_dec_conv(b2)
 
@@ -145,6 +96,5 @@ class SegNet(nn.Module):
         d0 = self.dec_conv0(self.upsample0(b3, ind3))
         d1 = self.dec_conv1(self.upsample1(d0, ind2))
         d2 = self.dec_conv2(self.upsample2(d1, ind1))
-        d3 = self.dec_conv3(self.upsample3(d2, ind0))  # no activation
-        #the output image is going to have the same number of dimensions as the input image
-        return d3
+        output = self.dec_conv3(self.upsample3(d2, ind0))  
+        return output 
