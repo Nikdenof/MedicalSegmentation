87621829b8ee31c603528e361fb372f0fac70260
Nikolai <nikdenof@gmail.com>
2023-03-11 00:46:12 +0300

    minor

--- hubconf.py
+++ hubconf.py
@@ -1,11 +1,14 @@
-dependencies = ['torch']
-from model_segnet import SegNet as _segnet
+import os
+import sys
+
+from model_segnet import SegNet as segnet
+#sys.path.append(os.path.join(sys.path[0], 'PyTorch/Detection/SSD'))
 
 # resnet18 is the name of entrypoint
-def segnet(**kwargs):
-    """ # This docstring shows up in hub.help()
-    SegNet model
-    """
-    # Call the model, load pretrained weights
-    model = _segnet(**kwargs)
-    return model
+# def segnet(**kwargs):
+#     """ # This docstring shows up in hub.help()
+#     SegNet model
+#     """
+#     # Call the model, load pretrained weights
+#     model = _segnet(**kwargs)
+#     return model
